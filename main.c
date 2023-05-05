9462e83d5923f43ac569e2571111bc16199a6f9d
Agustin Mansilla <mansillaagustin1019@gmail.com>
2023-05-04 21:33:26 -0300

    Storing user input into an array

--- main.c
+++ main.c
@@ -2,25 +2,73 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main (int argc, char **argv){
+int main (){
     char *prompt = "[My Shell] $ ";
-    char *lineptr;
+    char *full_command = NULL;
+    char *copy_command = NULL;
+    const char *delim = " \n";
     size_t n = 0;
     ssize_t nchars_read;
-
-    (void)argc;(void)argv;
+    char *token;
+    int num_tokens = 0;
+    int i = 0;
+    int counter;
+    
+    char **argv;
+    // (void)argc;
 
     while (1) {
+        num_tokens = 0;
         printf("%s",prompt);
-        nchars_read = getline(&lineptr, &n, stdin);
+        nchars_read = getline(&full_command, &n, stdin);
         if(nchars_read == -1){
             printf("Exiting shell...\n");
             return(-1);
         }
-        printf("%s", lineptr);
+        // Allocate space for the copy of the input user
+        copy_command = malloc(sizeof(char) * nchars_read);
+        if(copy_command == NULL){
+            printf("Memory allocation error");
+            return(-1);
+        }
+        // Copy the content into the new variable
+        strcpy(copy_command, full_command);
+
+        // Split the strings into tokens
+        token = strtok(full_command, delim);
+
+        // Count the number of tokens
+        while(token != NULL){
+            num_tokens++;
+            token = strtok(NULL,delim);
+        }
+        num_tokens++;
+
+        // Allocate memory for a array of strings
+        argv = malloc(sizeof(*argv) * num_tokens);
+
+        // Save each token in the array
+        token = strtok(copy_command, delim);
+        for(i = 0; token != NULL; i++){
+            argv[i] = malloc(sizeof(**argv) * strlen(token));
+            strcpy(argv[i],token);
+
+            token = strtok(NULL,delim);
+        }
+        argv[i] = NULL;
+        
+        for (counter = 0; counter<num_tokens-1; counter++){
+            printf("%s\n", argv[counter]);
+        }
+        // for(i = 0; i < num_tokens;i++){
+        //     free(argv[i]);
+        // }
+        free(argv);
+        free(copy_command);
 
     }
-    free(lineptr);
+    free(full_command);
     return 0;
 }
