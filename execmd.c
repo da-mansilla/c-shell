1036d160f1321ae92c777a240a610c73718381e0
Agustin Mansilla <mansillaagustin1019@gmail.com>
2023-05-11 18:39:09 -0300

    Execute multiple commands without quiting the shell

--- execmd.c
+++ execmd.c
@@ -1,18 +1,26 @@
 #include "main.h"
 #include <stdio.h>
+#include <sys/wait.h>
 #include <unistd.h>
-#include "main.h"
 
 void execmd(char **argv) {
+  int status;
+  pid_t pid;
+  char *command = NULL, *actual_command = NULL;
+
+  pid = fork();
+
   if (argv) {
-    char *command = NULL;
-    char *actual_command;
-
-    command = argv[0];
-    actual_command = get_location(command);
-
-    if (execve(actual_command, argv, NULL) == -1) {
-      perror("Error.");
+    if (pid < 0) {
+      perror("Error creating fork child");
+    } else if (pid == 0) {
+      command = argv[0];
+      actual_command = get_location(command);
+      if (execve(actual_command, argv, NULL) == -1) {
+        perror("Error.");
+      }
+    } else {
+      waitpid(pid, &status, 0);
     }
   }
 }
